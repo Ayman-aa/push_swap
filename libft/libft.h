e8207b0e60593b9570cd8151270008e60295bd5f
aaamam <aaamam@e2r6p9.1337.ma>
2024-02-02 10:39:56 +0100

    norm

---
+++ libft/libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaamam <aaamam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 10:20:20 by aaamam            #+#    #+#             */
+/*   Updated: 2024/02/02 10:28:42 by aaamam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+int		ft_strcmp(char *s1, char *s2);
+char	**ft_split(char const *s, char c);
+char	*get_next_line(int fd);
+char	*ft_strjoin1(char *line, char *buff);
+char	*ft_strchr1(char *s, int c);
+size_t	ft_strlen1(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+int		ft_strcmp(char *s1, char *s2);
+#endif
--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: aaamam <aaamam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 09:46:31 by aaamam            #+#    #+#             */
-/*   Updated: 2024/02/02 10:01:53 by aaamam           ###   ########.fr       */
+/*   Updated: 2024/02/02 10:39:27 by aaamam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 int	ft_atoi(char *str, int *stack)
 {
-    unsigned int    i;
-    int             sign;
-    unsigned int    result;
+	unsigned int	i;
+	int				sign;
+	unsigned int	result;
 
-    i = 0;
-    result = 0;
-    sign = 1;
-    while ((str[i] >= '9' && str[i] <= '13') || str[i] == ' ')
-        i++;
-    if (str[i] == '-')
-        sign = -1;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    while (str[i])
-    {
-        if (str[i] < '0' && str[i] > '9')
-            ft_error(stack);
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    if ((result > 2147483647 && sign == 1) 
-    || (result > 2147483648 && sign == -1))
-        ft_error(stack);
-    return (result * sign);
+	i = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] >= '9' && str[i] <= '13') || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' && str[i] > '9')
+			ft_error(stack);
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if ((result > 2147483647 && sign == 1)
+		|| (result > 2147483648 && sign == -1))
+		ft_error(stack);
+	return (result * sign);
 }
 
 int	ft_strlen(char **str)
@@ -51,4 +51,16 @@ int	ft_strlen(char **str)
 		i++;
 	}
 	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }

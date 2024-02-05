cde50f656315a88d48adba484fa80739402b2374
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2024-02-05 10:14:06 +0100

    adding swap

--- push.c
+++ push.c
@@ -1,26 +1,4 @@
-#include "../includes/push_swap.h"
-
-void push_b(t_stacks *stack, int param)
-{
-    int i;
-
-    if (stack->size_a)
-    {
-        stack->size_b++;
-        i = stack->size_b;
-        while (--i > 0)
-            stack->size_b[i] = stack->size_b[i - 1];
-        stack->size_b[0] = stack->size_a[0];
-        i = -1;
-        stack->size_a--;
-        while (++i < stack->size_a)
-            stack->size_a[i] = stack->size_a[i + 1];
-        if (!param)
-            write(1, "pb\n", 3);
-    }
-    else
-        return ;
-}
+#include "includes/push_swap.h"
 
 void push_a(t_stacks *stack, int param)
 {
@@ -39,6 +17,28 @@ void push_a(t_stacks *stack, int param)
             stack->size_b[i] = stack->size_b[i + 1];
         if (!param)
             write(1, "pa\n", 3);
+    }
+    else
+        return ;
+}
+
+void push_b(t_stacks *stack, int param)
+{
+    int i;
+
+    if (stack->size_a)
+    {
+        stack->size_b++;
+        i = stack->size_b;
+        while (--i > 0)
+            stack->size_b[i] = stack->size_b[i - 1];
+        stack->size_b[0] = stack->size_a[0];
+        i = -1;
+        stack->size_a--;
+        while (++i < stack->size_a)
+            stack->size_a[i] = stack->size_a[i + 1];
+        if (!param)
+            write(1, "pb\n", 3);
     }
     else
         return ;
---
+++ swap.c
@@ -0,0 +1,29 @@
+#include "includes/push_swap.h"
+
+void    swap_a(t_stacks *stack, int param)
+{
+    int tmp;
+
+    if (stack->size_a == 0 || stack->size_a == 1)
+        return ;
+    tmp = 0;
+    tmp = stack->size_a[0];
+    stack->size_a[0] = stack->size_a[1];
+    stack->size_a[1] = tmp;
+    if (!param)
+        write(1, "sa\n", 3);
+}
+
+void    swap_b(t_stacks *stack, int param)
+{
+    int tmp;
+
+    if (stack->size_b == 0 || stack->size_b == 1)
+        return ;
+    tmp = 0;
+    tmp = stack->size_b[0];
+    stack->size_b[0] = stack->size_b[1];
+    stack->size_b[1] = tmp;
+    if (!param)
+        write(1, "sb\n", 3);
+}

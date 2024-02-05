72a3cd49102a4f2f9cffe09ae4de823dba9630b5
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2024-02-05 10:30:25 +0100

    sawp sawp

--- swap.c
+++ swap.c
@@ -27,3 +27,21 @@ void    swap_b(t_stacks *stack, int param)
     if (!param)
         write(1, "sb\n", 3);
 }
+
+void    swap_s(t_stacks *stack, int param)
+{
+    int tmp;
+
+    if (stack->size_a == 0 || stack->size_a == 1
+        || stack->size_b == 0 || stack->size_b == 1)
+        return ;
+    tmp = 0;
+    tmp = stack->size_a[0];
+    stack->size_a[0] = stack->size_a[1];
+    stack->size_a[1] = tmp;
+    tmp = stack->size_b[0];
+    stack->size_b[0] = stack->size_b[1];
+    stack->size_b[1] = tmp;
+    if (!param)
+        write(1, "ss\n", 3);
+}

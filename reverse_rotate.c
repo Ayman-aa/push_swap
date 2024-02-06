c034338c08cf9c76ab2886de536b6acdb8d6bb48
aaamam <aaamam@c4r8p8.1337.ma>
2024-02-06 06:52:28 +0100

    norm

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -1,39 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaamam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 06:49:20 by aaamam            #+#    #+#             */
+/*   Updated: 2024/02/06 06:52:09 by aaamam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "inlclude/push_swap.h"
 
-void reverse_rotate_a(t_stacks *stack, int param)
+void	reverse_rotate_a(t_stacks *stack, int param)
 {
-    int tmp;
-    int i;
+	int	tmp;
+	int	i;
 
-    if (stack->size_a == 0 || stack->size_a == 1)
-    return ;
-i = stack->size_a -1;
-tmp = stack->size_a[i];
-while (i)
-{
-    stack->size_a[i] = stack-size_a[i - 1];
-    i--;
-}
-stack-size_a[i] = tmp;
-if (!param)
-    write(1, "rra\n", 4);
+	if (stack->size_a == 0 || stack->size_a == 1)
+		return ;
+	i = stack->size_a -1;
+	tmp = stack->size_a[i];
+	while (i)
+	{
+		stack->size_a[i] = stack->size_a[i - 1];
+		i--;
+	}
+	stack->size_a[i] = tmp;
+	if (!param)
+		write(1, "rra\n", 4);
 }
 
-void reverse_rotate_b(t_stacks *stack, int param)
+void	reverse_rotate_b(t_stacks *stack, int param)
 {
-    int tmp;
-    int i;
+	int	tmp;
+	int	i;
 
-    if (stack->size_b == 0 || stack->size_b == 1)
-    return ;
-i = stack->size_b - 1;
-tmp = stack->size_b[i];
-while (i)
-{
-stack->size_b[i] = stack->size_b[i - 1];
-i--;
-}
-stack->size_b[i] = tmp;
-if (!param)
-write(1, "rrb\n", 4);
+	if (stack->size_b == 0 || stack->size_b == 1)
+		return ;
+	i = stack->size_b - 1;
+	tmp = stack->size_b[i];
+	while (i)
+	{
+		stack->size_b[i] = stack->size_b[i - 1];
+		i--;
+	}
+	stack->size_b[i] = tmp;
+	if (!param)
+		write(1, "rrb\n", 4);
 }
